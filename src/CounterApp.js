39be4f15e6c6d444ff382249d4be8d20010a0967
Iván Bazaldúa <icesarbr@gmail.com>
2020-08-03 15:57:09 -0500

    My first simple react app with hooks

---
+++ src/CounterApp.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({value}) => {
+
+    const [counter, setCounter] = useState(value);
+
+    const handleAdd = (e) => {
+        setCounter(counter + 1);
+    };
+
+    const HandleReset = () => {
+        setCounter(value);
+    };
+
+    const HandleSubstract = () => {
+        setCounter(counter - 1);
+    };
+
+    return (
+        <>
+            <h1>Counter App</h1>
+            <h2>{counter}</h2>
+            <button onClick={ handleAdd } >+1</button>
+            <button onClick={HandleReset} >Reset</button>
+            <button onClick={HandleSubstract} >-1</button>
+        </>
+    )
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
+
+export default CounterApp;
---
+++ src/FirstApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FirstApp = (props) => {
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Mi primera App (:</p>
+        </>
+        );
+};
+
+export default FirstApp;
